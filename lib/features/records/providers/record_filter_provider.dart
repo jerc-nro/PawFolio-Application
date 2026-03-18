61978f3ff8dc51ef7d11d96bf9342918731e9ac7
jerc-nro <jxrc.rndm@gmail.com>
2026-03-19 05:25:39 +0800

    Refresh cache and upload all project files

--- lib/features/records/providers/record_filter_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final recordsTypeFilterProvider = StateProvider<String>((ref) => 'ALL');
-
-final recordsBreedFilterProvider = StateProvider<String>((ref) => 'ALL');
-
-final recordsSexFilterProvider = StateProvider<String>((ref) => 'ALL');
-
-final recordsSterilizedFilterProvider = StateProvider<bool?>((ref) => null);
--- lib/utils/constants/app_colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color accentRed = Color(0xFF7B2B2B);
-  static const Color background = Color(0xFFD7CCC8);
-  static const Color cardGrey = Color(0xFF949B86);
-  static const Color archiveBlue = Color(0xFF455A71);
-}
